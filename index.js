8008562474d3a3b20a96ec06c1f1727f02cdffc4
Shubham Kumar Saurav Dhiman <XDNR84@motorolasolutions.com>
2023-04-17 20:55:17 +0530

    Added Build folder

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.use(
 );
 
 if(process.env.NODE_ENV == 'production'){
-  app.use(express.static("client/build"))
+  app.use(express.static("Client/build"))
   const path = require("path")
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+    res.sendFile(path.resolve(__dirname, 'Client','build','index.html'))
   })
 }
 
